8c018d8ad5ffbe3f031a74c9a90e5db2f11da3b5
Aaron Hsu <aaron@nextbit.com>
2016-04-26 14:24:23 -0700

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var del = require('del');
+
+gulp.task('clean', function() {
+	return del.sync('build');
+});
+
+gulp.task('jade', function() {
+  return gulp.src('src/**/*.jade')
+  	.pipe(jade())
+  	.pipe(gulp.dest('build'))
+  	.pipe(browserSync.reload({
+		stream:true
+	}));
+});
+
+gulp.task('stylus', function() {
+  return gulp.src('src/**/*.styl')
+  	.pipe(stylus({
+        'include css': true
+    }))
+    .pipe(concat('out.css'))
+  	.pipe(gulp.dest('build'))
+  	.pipe(browserSync.reload({
+		stream:true
+	}));
+});
+
+gulp.task('move-index', function() {
+	return gulp.src('src/index.html')
+		.pipe(gulp.dest('build'))
+		.pipe(browserSync.reload({
+			stream:true
+		}));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+  	.pipe(gulp.dest('build/fonts'));
+});
+
+gulp.task('initBrowserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+  })
+});
+
+gulp.task('watch', ['initBrowserSync', 'jade', 'stylus', 'move-index'], function () {
+  gulp.watch('src/**/*.jade', ['jade']);
+  gulp.watch('src/**/*.styl', ['stylus']);
+  gulp.watch('src/index.html', ['move-index']);
+  gulp.watch('src/**/*.html', browserSync.reload);
+  gulp.watch('src/**/*.css', browserSync.reload);
+  gulp.watch('src/**/*.js', browserSync.reload);
+});
+
+gulp.task('update', ['move-index', 'jade', 'stylus']);
+gulp.task('default', ['clean', 'fonts', 'initBrowserSync', 'watch', 'update']);
+
+
